e4364c771cde3bd3e1401b0bc95dc1d5d0c1fce0
Liz Fonseca <lfonsec04@gmail.com>
2015-04-18 14:50:20 -0400

    updated files

--- app.js
+++ app.js
@@ -7,12 +7,17 @@ var methodOverride = require("method-override");
 var morgan = require("morgan");
 var Mustache = require("mustache");
 var sqlite3 = require("sqlite3");
+var util = require('util');
 // also needed
 var db = new sqlite3.Database("./forum.db");
 var app= express();
+  var topicsTemplate = fs.readFileSync('./views/topics.html', 'utf8');
+  var commentsTemplate = fs.readFileSync('./views/comments.html', 'utf8');
+  var topicForm = fs.readFileSync('./views/create_t.html', 'utf8');
+  var commentForm = fs.readFileSync('./views/create_c.html', 'utf8');
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static('Hunter_Chronicles'));
+// app.use(express.static('views'));
 app.use(methodOverride("_method"));
 app.use(morgan("dev"));
 // access index page
@@ -21,32 +26,32 @@ app.get('/', function(req, res){
 });
 // access main forum page
 app.get('/topics', function(req, res){
-  var template = fs.readFileSync('./views/topics.html', 'utf8');
   db.all("SELECT * FROM topics;", function(err, topics){
-    var html = Mustache.render(template, {topicsList: topics});
+    var html = Mustache.render(topicsTemplate, {topicsList: topics});
     res.send(html);
   });
 });
 // directs to the new topic form
 app.get('/topics/create', function(req, res){
-  console.log(req.body);
-  var markedTitle = marked("'" + req.body.title + "'");
-  var markedDescr = marked("'" + req.body.description + "'");
-  var markedAuthor = marked("'" + req.body.author + "'");
-  res.send(fs.readFileSync('./views/create_t.html', 'utf8'));
-    db.run("INSERT INTO topics (Title, Description, Author, Votes) VALUES ('" + markedTitle + "', '" + markedDescr + "', '" + markedAuthor + "', 0);");
-// NEED TO FIX ISSUE WHERE ENPTY FORM FIELDS GET ADDED
-    db.run("DELETE FROM topics WHERE Author='undefined';");
+   console.log(req.body);
+  res.send(topicForm);
 });
+  // NEED TO FIX ISSUE WHERE ENPTY FORM FIELDS GET ADDED
+
 // redirects user to main forum page
 app.post('/topics', function(req, res){
-  var template = fs.readFileSync('./views/topics.html', 'utf8');
-  db.all("SELECT * FROM topics;", function(err, topics){
-    var html = Mustache.render(template, {topicsList: topics});
-    res.send(html);
-  });
+  console.log(req.body);
+  db.run("INSERT INTO topics (title, description, author, votes) VALUES ('" + req.body.title + "', '" + req.body.description + "', '" + req.body.author + "', 0);");
+  // res.send('/topics');
 });
-
+// directs user to comments of a topic
+app.get('/topics/:id/comments', function(req, res){
+  var id = req.params.id;
+  db.all("SELECT FROM comments WHERE topics_id=" + id, function(err, topics){
+    var html = Mustache.render(commentsTemplate, {commentDetails: comments});
+    res.send(html);
+    });
+});
 // runs server
 app.listen(2000, function(){
   console.log("The server listens...");
--- schema.js
+++ schema.js
@@ -1,6 +1,6 @@
 var sqlite3 = require("sqlite3");
 var db = new sqlite3.Database("./forum.db");
 // Table structure for Topics
-db.run("CREATE TABLE topics (ID INTEGER PRIMARY KEY AUTOINCREMENT, Title text, Author varchar(255), Description text, Votes integer);");
+db.run("CREATE TABLE topics (id INTEGER PRIMARY KEY AUTOINCREMENT, title text, author varchar(255), description text, votes integer);");
 // Table structure for Comments
-db.run("CREATE TABLE comments (ID INTEGER PRIMARY KEY AUTOINCREMENT, Title text, Author varchar(255), Content text, City varchar(255), Topics_ID integer, FOREIGN KEY(Topics_ID) REFERENCES topics(ID));");
+db.run("CREATE TABLE comments (id INTEGER PRIMARY KEY AUTOINCREMENT, title text, author varchar(255), content text, city varchar(255), topics_id integer, FOREIGN KEY(topics_id) REFERENCES topics(ID));");
