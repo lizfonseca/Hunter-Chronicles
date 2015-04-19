d6924f4e7d4dc8d272781235a8928010d8ac30ca
Liz Fonseca <lfonsec04@gmail.com>
2015-04-19 15:54:40 -0400

    fixing and breaking

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app= express();
   var topicsTemplate = fs.readFileSync('./views/topics.html', 'utf8');
   var commentsTemplate = fs.readFileSync('./views/comments.html', 'utf8');
   var topicForm = fs.readFileSync('./views/create_t.html', 'utf8');
-  var commentForm = fs.readFileSync('./views/create_c.html', 'utf8');
+  // var commentForm = fs.readFileSync('./views/create_c.html', 'utf8');
 
 app.use(bodyParser.urlencoded({extended: false}));
 // app.use(express.static('views'));
@@ -36,21 +36,71 @@ app.get('/topics/new', function(req, res){
    console.log(req.body);
   res.send(topicForm);
 });
-  // NEED TO FIX ISSUE WHERE ENPTY FORM FIELDS GET ADDED
+// NEED TO FIX ISSUE WHERE ENPTY FORM FIELDS GET ADDED
 // saves info into db & redirects user to main forum page
 app.post('/topics/create', function(req, res){
   console.log(req.body);
   db.run("INSERT INTO topics (title, description, author, votes) VALUES ('" + req.body.title + "', '" + req.body.description + "', '" + req.body.author + "', 0);");
   res.redirect('/topics');
 });
-// directs user to comments of a topic
-app.get('/topics/:id/comments', function(req, res){
+// directs user to the comments of a topic
+// the user can also read comments and post a new comment on the form
+app.get('topics/:id', function(res, req){
+  // console.log(res)
   var id = req.params.id;
-  db.all("SELECT FROM comments WHERE topics_id=" + id, function(err, comments){
-    var html = Mustache.render(commentsTemplate, {commentDetails:comments});
-    res.send(html);
+  db.all("SELECT * FROM topics WHERE id=" + id + ";", {}, function(err, topic){
+    db.all("SELECT * FROM comments WHERE topic_id=" + id + ";", {}, function(err, comments){
+      fs.readFileSync('./views/comments.html', 'utf8', function(err, content){
+        var html = Mustache.render(commentsTemplate, {
+          // id: topic[0].id,
+          title: topic[0].title,
+          votes: topic[0].votes,
+          description: topic[0].description,
+          commentDetails: comments
+        });
+        res.send(html);
+      });
     });
+  });
 });
+
+
+app.get('/topics/:id', function(req, res){
+  var id = req.params.id;
+  db.all("SELECT * FROM topics WHERE id =" + id + ";", {}, function(err, topic){
+    db.all("SELECT * FROM comments WHERE topics_id=" + id + ";", {}, function(err, content){
+      fs.readFileSync('./views/comments.html', 'utf8', function(err, comments){
+    var html = Mustache.render(commentsTemplate, {
+      // title: topic[0].title,
+      // votes: topic[0].votes,
+      // description: topic[0].description,
+      commentDetails:comments});
+    res.send(html);
+      });
+    });
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// add new comment into specific topic
+// app.post('topics/:id', function(req, res){
+//   var id = req.params.id;
+//   db.all("SELECT FROM topics WHERE id = " + id +";", {}, function(err, topic){
+//     db.all("INSERT INTO comments (title, author, content) VALUES ")
+//   }){
+
+//   };
+// };
 // runs server
 app.listen(2000, function(){
   console.log("The server listens...");
