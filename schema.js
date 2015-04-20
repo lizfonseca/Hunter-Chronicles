d012f67bcad74def956bb6bd808d6ca0fcc0e2e3
lizfonseca <lfpauthz@gmail.com>
2015-04-20 10:37:29 -0400

    Delete schema.js

--- schema.js
+++
@@ -1,6 +0,0 @@
-var sqlite3 = require("sqlite3");
-var db = new sqlite3.Database("./forum.db");
-// Table structure for Topics
-db.run("CREATE TABLE topics (id INTEGER PRIMARY KEY AUTOINCREMENT, title text, author varchar(255), description text, votes integer);");
-// Table structure for Comments
-db.run("CREATE TABLE comments (id INTEGER PRIMARY KEY AUTOINCREMENT, title text, author varchar(255), content text, city varchar(255), topics_id integer, FOREIGN KEY(topics_id) REFERENCES topics(ID));");
